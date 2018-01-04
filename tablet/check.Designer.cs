b38cd22667394a5bed88eaba6c0d05198ec17332
Emre YÜZ <emreyuz@msn.com>
2018-01-04 17:29:49 +0300

    Add files via upload

---
+++ tablet/check.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace tablet
+{
+    partial class check
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(check));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxCheck = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCheck)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 300000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Program Çalışıyor";
+            // 
+            // pictureBoxCheck
+            // 
+            this.pictureBoxCheck.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxCheck.Image")));
+            this.pictureBoxCheck.Location = new System.Drawing.Point(13, 23);
+            this.pictureBoxCheck.Name = "pictureBoxCheck";
+            this.pictureBoxCheck.Size = new System.Drawing.Size(16, 16);
+            this.pictureBoxCheck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCheck.TabIndex = 1;
+            this.pictureBoxCheck.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(189, 16);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 30);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Başlat";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(270, 16);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 30);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Durdur";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // check
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 58);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBoxCheck);
+            this.Controls.Add(this.label1);
+            this.Name = "check";
+            this.Text = "Controller";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCheck)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxCheck;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ tablet/check.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace tablet
+{
+    public partial class check : Form
+    {
+        public check()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (var process in Process.GetProcessesByName("WTClient"))
+                {
+                    process.Kill();
+                }
+
+                System.Diagnostics.Process.Start(@"C:\Windows\SysWOW64\WTClient.exe");
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timer.Start();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+    }
+}
